67228e945596240173cdd845d200ef8db764d05a
Lars Gunther <gunther@keryx.se>
2016-08-07 13:19:31 +0200

    Toggle-menu stub

---
+++ html/scripts/toggle-menu.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+    // Skapa knappen som visar/döljer menyn
+    // Lagra åtkomst till knappen i en variabel
+    // Lagra åtkonst till menyn i en variabel
+    // När någon klickar på knappen
+        // Om menyn redan visas
+            // Ska den döljas
+            // Och ändra knappens text
+            // Annars ska den visas
+            // Och knappens text ändras
+}());
