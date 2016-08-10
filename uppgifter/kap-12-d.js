a15d28a5c29f6709479ae071639cb4b40ecf319b
Lars Gunther <gunther@keryx.se>
2016-08-10 17:52:42 +0200

    Assignment 12D JS comments

--- uppgifter/kap-12-d.js
+++ uppgifter/kap-12-d.js
@@ -1,9 +1,18 @@
 ( () => {
     // ES 6 arrow function
     var pcells = document.querySelectorAll("td:last-child");
+    // Variabeln pcells är en array med de sista cellerna i varje rad
+    // Loopa igenom denna array
     for (var pc of pcells ) {
+        // pc är en enskild cell i listan
+        // sp är cellen före, semantikpoängen
         var sp = pc.previousElementSibling;
+        // vp är cellen före sp, valideringspoängen
         var vp = sp.previousElementSibling;
-        pc.innerHTML = +vp.innerText + +sp.innerText;
+        // Summera värdet matematiskt
+        // De extra plustecknen förhindrar att "7" + "8" = "78"
+        // utan att det blir 7 + 8 = 15
+        // Lagra summan som text i cellen pc
+        pc.innerText = +vp.innerText + +sp.innerText;
     }
 })(); // Self executing function
